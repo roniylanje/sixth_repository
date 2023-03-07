f772a8b4a6b4151d910355a62ed45d04221f89e5
roniylanje <rohitlanje7@gmail.com>
2023-03-07 19:31:31 +0530

    this is for u

---
+++ list_programme.py
@@ -0,0 +1,11 @@
+'''list_1 = ["apple","nokia","oppo","oneplus","samsung"]
+list_1.remove("samsung")
+print (list_1)'''
+
+'''thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print (thislist)'''
+
+thislist1 = ["apple", "banana", "cherry"]
+thislist1.pop (1)
+print (thislist1)
